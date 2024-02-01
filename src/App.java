6714a21a5f9c6df9f16984e8de8a16613acac41d
pedroportales <pedroportales@riseup.net>
2024-02-01 14:36:40 -0300

    area do quadrado

--- src/App.java
+++ src/App.java
@@ -1,7 +1,34 @@
+class Quadrado{
+    // Atributos
+    private int lado;
 
+    // Métodos
+    Quadrado(int lado){
+        this.lado = lado;
+    }
+
+    public int getLado(){
+        return lado;
+    }
+
+    public void setLado(int lado){
+        this.lado= lado;
+    }
+
+    public int calcularArea(){
+        return lado * 4;
+    }
+}
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Quadrado quadrado1 = new Quadrado(5);
+        System.out.println("Lado do quadrado: " + quadrado1.getLado());
+        System.out.println("Área do quadrado: " + quadrado1.calcularArea());
+
+        quadrado1.setLado(10);
+        System.out.println("Lado do quadrado: " + quadrado1.getLado());
+        System.out.println("Área do quadrado: " + quadrado1.calcularArea());
+
     }
 }
